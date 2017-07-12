ba4f5264c22254dd5a84643a005a6197bd23611d
k-water <625592890@qq.com>
2017-07-12 16:29:37 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/variable'
+
+  #app
+    color: $color-theme
+</style>
