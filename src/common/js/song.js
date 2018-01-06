ab43d4e067e36d9c8c441f5bae8ea9f44bd489a0
k-water <625592890@qq.com>
2018-01-06 08:33:04 +0800

    fix: update music source

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -42,7 +42,7 @@ export function createSong(musicData) {
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
     // 歌源url
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `http://thirdparty.gtimg.com/C100${musicData.songmid}.m4a?fromtag=38`
   })
 }
 
